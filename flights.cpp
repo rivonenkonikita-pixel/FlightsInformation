b9f6a164cc3692cb82d5a995fd7da05d3d527e43
feinfly <rivonenkonikita@gmail.com>
2026-05-26 23:31:22 +0300

    change flights.cpp

--- flights.cpp
+++ flights.cpp
@@ -140,7 +140,6 @@ int ChooseOption()
         cout << "Такого варианта обработки данных нет. Повторите попытку, пожалуйста" << endl;
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << endl;
     } while (true);
     cout << endl << "Выбран вариант " << option << endl << endl;
     return option;
@@ -235,7 +234,6 @@ int ReadLine(ifstream& file, FlightInformation& flight, bool printline, int line
         }
         file.clear();
         file.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << endl;
         return 20;
     }
     if (printline)
