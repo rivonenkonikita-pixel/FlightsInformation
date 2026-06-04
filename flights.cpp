ad0eb4ab3bb6a82f0b760db8ca73ebad2fb4cf84
feinfly <rivonenkonikita@gmail.com>
2026-06-04 09:41:58 +0300

    changes

--- flights.cpp
+++ flights.cpp
@@ -27,7 +27,7 @@
 #include <limits> //Содержит информацию о пределах типов данных,
 using namespace std;
 
-const char* FILE_NAME = "data01.txt"; //Имя файла
+const char* FILE_NAME = "data04.txt"; //Имя файла
 const int MAX_LINE_LEN = 1000; //Максимальная длина одной строки в файле
 const int MAX_TAIL_LEN = 100; //Максимальная длина бортового номера
 
